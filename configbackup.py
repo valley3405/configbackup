acb39ffa912466767eb1d796e69c761da291fc32
valley <212125@qq.com>
2015-02-01 19:36:34 +0800

    small update

--- configbackup.py
+++ configbackup.py
@@ -9,8 +9,13 @@ import os
 host = '10.252.21.254'
 username = 'root'
 password = 'tjkj@1216'
+command1 = 'display current'
 
 foo = pexpect.spawn('/usr/bin/ssh %s@%s' % (username, host))
 foo.expect('.*ssword:')
 foo.sendline(password)
-foo.interact()
+#foo.interact()
+foo.sendline(command1)
+foo.sendline('                                                       ')
+foo.expect('>')
+foo.expect()
