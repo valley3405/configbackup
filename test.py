954d3cdf8666c27caf2caf3a923dc3bfb78e15bb
valley <212125@qq.com>
2015-02-01 16:26:25 +0800

    test chinese

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#-- coding:utf-8 --
 print "hello~world"
 print "test 1"
 print "test 2"
@@ -6,3 +7,4 @@ print "test 3"
 print "test 4"
 print "test 5"
 print "test 6"
+print u"中国"
