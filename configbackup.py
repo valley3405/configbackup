8f6faa41659b9a909536cfc8b41a1477eb4636be
valley <212125@qq.com>
2015-02-01 19:57:47 +0800

    small update

--- configbackup.py
+++ configbackup.py
@@ -6,16 +6,21 @@ import sys
 import time  
 import os  
 
+outputfile = '1.out'
 host = '10.252.21.254'
 username = 'root'
 password = 'tjkj@1216'
 command1 = 'display current'
 
+fout = open(outputfile,'w')
+fout.write ('==========Log Tile: Auto config backup==========\n')
+
 foo = pexpect.spawn('/usr/bin/ssh %s@%s' % (username, host))
+foo.logfile = fout
 foo.expect('.*ssword:')
 foo.sendline(password)
 foo.sendline(command1)
 foo.sendline('                                                       ')
 foo.expect('>')
 
-foo.interact()
+#foo.interact()
