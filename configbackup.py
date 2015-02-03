61893eacf94259eff6f46ca83e0c7385f9f3a9ff
valley <212125@qq.com>
2015-02-03 23:26:02 +0800

    small update

--- configbackup.py
+++ configbackup.py
@@ -8,33 +8,39 @@ import os
 import logging
 logging.basicConfig(level=logging.ERROR) 
 
-outputfile = '1.out'
-host = '10.252.21.254'
-username = 'root'
-password = 'tjkj@1216'
-commandlist = ['dis ip int brief','dis ip rout','display current','quit']
 
-fout = open(outputfile,'w')
-fout.write ('==========Log Tile: Auto config backup==========\n')
+#host = '10.252.21.254'
+#username = 'root'
+#password = 'tjkj@1216'
+#commandlist = ['dis ip int brief','dis ip rout','display current','quit']
 
-#-----send password to login------
-foo = pexpect.spawn('/usr/bin/ssh %s@%s' % (username, host))
-foo.logfile_read = fout
-foo.expect('.*ssword:')
-foo.sendline(password)
+def configbackup(host, username, password, commandlist[]):
+	outputfile = '1.out'
+	fout = open(outputfile,'w')
+	fout.write ('==========Log Tile: Auto config backup==========\n')
+	
+	#-----send password to login------
+	foo = pexpect.spawn('/usr/bin/ssh %s@%s' % (username, host))
+	foo.logfile_read = fout
+	foo.expect('.*ssword:')
+	foo.sendline(password)
+	
+	#-----send commands--------
+	for command in commandlist:
+		foo.expect('>')
+		logging.info(command+'\n')
+		foo.sendline(command)
+		foo.sendline('                                                       ')
+	
+	#-----end------------------
+	try:
+		foo.expect('pexpect.EOF')
+	except pexpect.EOF:
+		logging.info("End of the config\n")
+	fout.close()
 
-#-----send command1--------
-for command in commandlist:
-	foo.expect('>')
-	logging.info(command+'\n')
-	foo.sendline(command)
-	foo.sendline('                                                       ')
+def main():
+	configbackup('10.252.21.254', 'root', 'tjkj@1216', ['dis ip int brief','dis ip rout','display current','quit'])
 
-#-----end------------------
-try:
-	foo.expect('pexpect.EOF')
-except pexpect.EOF:
-	logging.info("End of the config\n")
-fout.close()
-
-#foo.interact()
+if __name__ == '__main__':
+	main()
