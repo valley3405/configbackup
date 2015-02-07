622788364f5283cb46f0c23fefec01764034a432
valley <212125@qq.com>
2015-02-07 10:44:54 +0800

    small update

--- configbackup.py
+++ configbackup.py
@@ -61,7 +61,7 @@ def main():
 
 	for host in hosts:
 		logging.info(host['hostip'])
-		configbackup(host['hostip'], host['username'], host['password'], modules[0][host['modulename']]) 
+		#configbackup(host['hostip'], host['username'], host['password'], modules[0][host['modulename']]) 
 	
 		#configbackup('10.252.21.254', 'root', 'tjkj@1216', ['dis ip int brief','dis ip rout','display current','quit'])
 
