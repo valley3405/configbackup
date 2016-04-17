1a532b3444563e12419b47c7d3d967e0128a6187
valley <212125@qq.com>
2016-04-17 22:58:32 +0800

    add comment

--- configbackup.py
+++ configbackup.py
@@ -55,20 +55,24 @@ def configbackup(dirstr, area, hostname, host, username, password, module):
 
 if __name__ == '__main__':
 
+	#指定脚本执行目录，从配置文件hosts.conf中读取设备清单，从modules.conf中读取执行命令集(针对不同设备)，解析json,并放入字典
 	prefix = '/root/configbackup/'
 	jsonf = open(prefix + 'hosts.conf')
 	hosts = json.loads(jsonf.read())
 	jsonf = open(prefix + 'modules.conf')
 	modules = json.loads(jsonf.read())
 
+	#取得当前时间，并组成由备份主目录+当前时间组合成的备份目录字符串，用于存放备份文件
 	now = datetime.datetime.now()
 	timestr = now.strftime("%Y-%m-%d_%H%M%S")
 	dirstr = prefix + "configBackup.d/" + timestr
 
+	#枚举hosts，针对每台设备，调用函数configbackup,执行对应命令集，保存结果，在指定目录生成备份文件
 	for host in hosts:
 		logging.info("--------Begin of backup of " + host['hostip'] + "-----------")
 		configbackup(dirstr, host['area'], host['hostname'], host['hostip'], host['username'], host['password'], modules[0][host['modulename']]) 
 
+	#执行操作系统指令svn，将备份文件更新入svn服务器
 	#svn commit -m "timestr" configBackup.d/
 	os.system("/usr/bin/svn update "+ prefix + "configBackup.d/")
 	os.system("/usr/bin/svn add "+dirstr)
